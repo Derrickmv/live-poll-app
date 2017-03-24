7221e3c42510a19162de81d539bdb1d26120ccf3
Derrickmv <derrick_8mv@yahoo.com>
2017-03-23 17:16:37 -0700

    removed port num

--- app-server.js
+++ app-server.js
@@ -18,7 +18,7 @@ var server = app.listen(process.env.PORT || 3000);
 
 
 app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, '/index.html'));
+	res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 
