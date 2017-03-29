52072693a1b94963aba4063f98f5311f00b2eca7
Derrickmv <derrick_8mv@yahoo.com>
2017-03-29 16:39:26 -0700

    Yess

--- app-questions.js
+++ app-questions.js
@@ -12,19 +12,5 @@ module.exports = [
         "b": "Reverse Flash",
         "c": "Zoom",
         "d": "Wally West"
-    },
-    {
-        "q": "How much money would you like to put down?",
-        "a": "more money than I have",
-        "b": "all of my money",
-        "c": "some of my money",
-        "d": "no money at all"
-    },
-    {
-        "q": "Who do you want to be your boss?",
-        "a": "someone bossy",
-        "b": "someone who knows what they are doing",
-        "c": "I'm my own boss",
-        "d": "no boss at all"
     }
 ];
