16a42c118a9662d0bf0fd97bf7762187c3f95fcb
Derrickmv <derrick_8mv@yahoo.com>
2017-03-29 15:15:10 -0700

    change questions

--- app-questions.js
+++ app-questions.js
@@ -1,17 +1,17 @@
 module.exports = [
     {
-        "q": "How much of your true potential are you using?",
-        "a": "none of my true potential",
-        "b": "some of my true potential",
-        "c": "most of my true potential",
-        "d": "all of my true potential"
+        "q": "Who is the best superhero?",
+        "a": "Batman",
+        "b": "Spider-Man",
+        "c": "Superman",
+        "d": "Wolverine"
     },
     {
-        "q": "How much money would you like to make?",
-        "a": "no money",
-        "b": "some money",
-        "c": "most of the money",
-        "d": "all of the money"
+        "q": "Who is faster?",
+        "a": "The Flash",
+        "b": "Reverse Flash",
+        "c": "Zoom",
+        "d": "Wally West"
     },
     {
         "q": "How much money would you like to put down?",
