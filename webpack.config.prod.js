ef178bc30164f807147db7d20bce5812f3c22e31
Derrickmv <derrick_8mv@yahoo.com>
2017-03-29 11:13:57 -0700

    changing node env from development back to production

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -25,7 +25,7 @@ export default {
     }),
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': JSON.stringify('development')
+        'NODE_ENV': JSON.stringify('production')
       }
     })
   ],
